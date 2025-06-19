885bc841fd710a7c4cd8d923c114f2976bf1bf8e
Sidharth-2 <sidharthtvr@gmail.com>
2025-06-19 21:10:54 +0530

    Google login

--- AuthenticationJwt/src/main/java/com/sid/AuthenticationJwt/repository/UserRepository.java
+++ AuthenticationJwt/src/main/java/com/sid/AuthenticationJwt/repository/UserRepository.java
@@ -6,7 +6,7 @@ import com.sid.AuthenticationJwt.entity.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 
     boolean existsByEmail(String email);
     
